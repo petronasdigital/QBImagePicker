629255339a487cdbbd21819ad7e1f3a5fbfb2b84
Faizal Sidek <faizal.sidek@gmail.com>
2020-06-29 14:31:27 +0800

    Force use potrait orientation.

--- QBImagePicker/QBAssetsViewController.m
+++ QBImagePicker/QBAssetsViewController.m
@@ -649,13 +649,7 @@ static CGSize CGSizeScale(CGSize size, CGFloat scale) {
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSUInteger numberOfColumns;
-    if (UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation])) {
-        numberOfColumns = self.imagePickerController.numberOfColumnsInPortrait;
-    } else {
-        numberOfColumns = self.imagePickerController.numberOfColumnsInLandscape;
-    }
-    
+    NSUInteger numberOfColumns self.imagePickerController.numberOfColumnsInPortrait;    
     CGFloat width = (CGRectGetWidth(self.view.frame) - 2.0 * (numberOfColumns - 1)) / numberOfColumns;
     
     return CGSizeMake(width, width);
